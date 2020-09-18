7e90a5fe4874f954096dd60b4b25f2184b9acc36
nickherrig <neherrig@gmail.com>
2020-09-17 23:06:41 -0500

    Parametize test for checksum

--- tests/test_serial.py
+++ tests/test_serial.py
@@ -1,6 +1,9 @@
 import pytest
 from pyservo.serial import *
 
-def test_calculate_checksum():
-    packet = bytearray([0x00, 0x00, 0x00])
-    assert calculate_checksum(packet) == 0x80
+@pytest.mark.parametrize(
+    "test_packet,                     expected_checksum",[
+     (bytearray([0x00, 0x00, 0x00]),  0x80)
+])
+def test_calculate_checksum(test_packet, expected_checksum):
+    assert calculate_checksum(test_packet) == expected_checksum
