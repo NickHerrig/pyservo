5e56a2fca103850dd341e07abaa5f794488d8dda
nickherrig <neherrig@gmail.com>
2020-09-20 17:17:41 -0500

    Started servo status parser

--- pyservo/read.py
+++ pyservo/read.py
@@ -1,28 +1,56 @@
 
-#TODO: Implement status data parser - def  parse_status(packet):
+def  parse_status_data(data_byte):
+    """
+    Documentation: http://www.dmm-tech.com/Files/DYN4MS-ZM7-A10A.pdf
+    Page: 53
+
+    Status = x b6 b5 b4 b3 b2 b1 b0
+
+    b0 = 0 : On position, i.e. |Pset - Pmotor| < = OnRange
+    b0 = 1 : motor busy, or |Pset - Pmotor|> OnRange
+    b1 = 0 : motor servo
+    b1 = 1 : motor free
+    b4 b3 b2 = 0 : No alarm
+     1 : motor lost phase alarm, |Pset - Pmotor|>8192(steps), 180(deg)
+     2 : motor over current alarm
+     3 : motor overheat alarm, or motor over power
+     4 : there is error for CRC code check, refuse to accept current command
+     5~ 7 : TBD
+    b5 = 0 : means buit in S-curve, linear, circular motion completed; waiting for next motion
+    b5 = 1 : means buit in S-curve, linear, circular motion is busy on current motion
+    b6 : pin2 status of JP3,used for Host PC to detect CNC zero position or others
+    """
+    servo_status = {}
+    if (data_byte >> 6) & 1 == 0:
+        servo_status['position'] = 'motor on position'
+    elif (data_byte >> 6) & 1 == 1:
+        servo_status['position'] = 'motor is busy'
+
 
 #TODO: Implement config data parser - def  parse_config(packet):
 
 #TODO: Implement abs data parser - def  parse_config(packet):
 
-
 read_func_codes_dict = {
     'Is_SpeedGain': 0x11,
+    'Is_Status':    0x19,
 }
 
 
 def  parse_data(packet):
     func_code = packet[1] & 0x1f
-    if func_code == 0x19:
-        print("Is_Status parser not implemented yet.")
-    elif func_code == 0x1a:
-        print("Is_Config  parser not implemented yet.")
-    elif func_code == 0x1b:
-        print("Absolute Position parser not implemented yet.")
-    elif func_code == 0x1e:
-        print("Current Torque parser not implemented yet.")
+    if func_code == read_func_codes_dict['Is_Status']:
+        return parse_status_data(packet[2])
+    # elif func_code == 0x1a:
+    #     print("Is_Config  parser not implemented yet.")
+    # elif func_code == 0x1b:
+    #     print("Absolute Position parser not implemented yet.")
+    # elif func_code == 0x1e:
+    #    print("Current Torque parser not implemented yet.")
     elif func_code in read_func_codes_dict.values():
         return packet[2] & 0b01111111
+    else:
+        return "Cannot recognized function code:{func_code}".format(func_code=func_code)
 
 
 def validate_checksum(packet):
