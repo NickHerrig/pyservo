d0f4d76ca7ad5df14b865a7e688b0c81739d0ae8
nickherrig <neherrig@gmail.com>
2020-09-29 22:08:25 -0500

    Add signed data test

--- tests/test_read.py
+++ tests/test_read.py
@@ -4,54 +4,61 @@ from pyservo.read import *
 
 @pytest.mark.parametrize(
     "test_byte,     expected_integer",[
-     (0b11111111,  -1),
-     (0b01111111,  127),
-     (0b10000010,  -126),
-     (0b00000010,  2),
+    (0b11111111,    -1),
+    (0b01111111,    127),
+    (0b10000010,    -126),
+    (0b00000010,    2),
 ])
 def test_sign_extend(test_byte, expected_integer):
     assert sign_extend(test_byte, 8) == expected_integer
 
+@pytest.mark.parametrize(
+    "test_data_bytes,         expected_integer",[
+    (bytearray(b'\xd0\xc2'),  -6078),
+])
+def test_parse_signed_data(test_data_bytes, expected_integer):
+    assert parse_signed_data(test_data_bytes) == expected_integer
+
 
 @pytest.mark.parametrize(
     "test_status_byte, expected_status",[
-     (0b10000000,      {
+    (0b10000000,       {
                            'pin2': '0',
                            'motion':'completed',
                            'alarm': 'no alarm',
                            'motor': 'servo',
                            'position': 'on position',}),
-     (0b11100111,      {
+    (0b11100111,       {
                            'pin2': '1',
                            'motion':'busy',
                            'alarm': 'lost phase',
                            'motor': 'free',
                            'position': 'busy',}),
-     (0b11101011,      {
+    (0b11101011,       {
                            'pin2': '1',
                            'motion':'busy',
                            'alarm': 'over current',
                            'motor': 'free',
                            'position': 'busy',}),
-     (0b11101111,      {
+    (0b11101111,       {
                            'pin2': '1',
                            'motion':'busy',
                            'alarm': 'overheat/overpower',
                            'motor': 'free',
                            'position': 'busy',}),
-     (0b11110011,      {
+    (0b11110011,       {
                            'pin2': '1',
                            'motion':'busy',
                            'alarm': 'rcr error',
                            'motor': 'free',
                            'position': 'busy',}),
-     (0b11110111,      {
+    (0b11110111,       {
                            'pin2': '1',
                            'motion':'busy',
                            'alarm': 'TBD',
                            'motor': 'free',
                            'position': 'busy',}),
-     (0b11111011,      {
+    (0b11111011,       {
                            'pin2': '1',
                            'motion':'busy',
                            'alarm': 'TBD',
@@ -69,8 +76,8 @@ def test_parse_status_data(test_status_byte, expected_status):
 
 @pytest.mark.parametrize(
     "test_packet,                      valid_checksum",[
-     (bytearray(b'\x02\x91\x85\x98'),  True),
-     (bytearray(b'\x02\x91\x85\x88'),  False),
+    (bytearray(b'\x02\x91\x85\x98'),  True),
+    (bytearray(b'\x02\x91\x85\x88'),  False),
 ])
 def test_validate_checksum(test_packet, valid_checksum):
     assert validate_checksum(test_packet) == valid_checksum
